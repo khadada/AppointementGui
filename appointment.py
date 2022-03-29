2ab00d2e2e36636b9ec7d922d417a3d2e5d6f79f
khadada <lkhadada@gmail.com>
2022-03-29 11:03:13 +0200

    Importing necessay modules and classes for this GUI

---
+++ appointment.py
@@ -0,0 +1,5 @@
+# Importing necessary modules and classes
+import sys
+from PyQt5.QtWidgets import (QApplication, QLabel, QPushButton, QWidget, QSpinBox, QHBoxLayout, QVBoxLayout, QComboBox)
+from PyQt5.QtGui import QFont
+from PyQt5.QtCore import Qt
