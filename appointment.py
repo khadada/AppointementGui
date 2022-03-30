433b8899a7ac16f6ad56ba7ec779cd4319f3744e
khadada <lkhadada@gmail.com>
2022-03-30 09:43:55 +0200

    I finish create initialize_ui() method

--- appointment.py
+++ appointment.py
@@ -4,3 +4,23 @@ from PyQt5.QtWidgets import (QApplication, QLabel, QPushButton, QWidget, QSpinBo
 from PyQt5.QtGui import QFont
 from PyQt5.QtCore import Qt
 
+# Build our class for the program
+class BookAppForm(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initialize_ui()
+        self.show()
+        
+    def initialize_ui(self):
+        """
+        Initialize the window and display its content to the screen.
+        """
+        self.setGeometry(100, 50, 300,400)
+        self.setWindowTitle("4.3 - Appointement Form Gui")
+        self.display_widgets()
+    
+    def display_widgets(self):
+        """
+        Here we create the necessary widgets for theis application
+        """
+        
