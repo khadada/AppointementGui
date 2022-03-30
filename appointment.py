130d10006bd2e838f75f694220d922593bc872bb
khadada <lkhadada@gmail.com>
2022-03-30 09:35:48 +0200

    Today I will do my best to finish this projet

--- appointment.py
+++ appointment.py
@@ -3,3 +3,4 @@ import sys
 from PyQt5.QtWidgets import (QApplication, QLabel, QPushButton, QWidget, QSpinBox, QHBoxLayout, QVBoxLayout, QComboBox)
 from PyQt5.QtGui import QFont
 from PyQt5.QtCore import Qt
+
